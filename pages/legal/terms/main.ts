ce50ee9734e9ed4dc58dd79214d4aa7e5c05806e
Mikhail Matiunin <mikhail.matiunin@doctorina.com>
2026-03-19 22:10:11 +0400

    Add legal script

--- pages/legal/terms/main.ts
+++
@@ -1,6 +0,0 @@
-import { initLegalPage } from '~/shared/legal/legal-page';
-
-initLegalPage({
-  localeModules: import.meta.glob('./locales/*.ts'),
-  localeNames: { en: 'English', es: 'Español' },
-});
